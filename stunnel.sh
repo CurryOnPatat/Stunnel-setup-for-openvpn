ee4603906b1a96a470ec82820043a117282ab514
CurryOnPatat <99186666+CurryOnPatat@users.noreply.github.com>
2022-09-14 14:54:02 +0200

    Update stunnel.sh

--- stunnel.sh
+++ stunnel.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-
 sudo apt install -y stunnel4
 cd /etc/stunnel/
 openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=NL' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
@@ -10,7 +8,6 @@ echo "[openvpn]" | sudo tee -a /etc/stunnel/stunnel.conf
 echo "accept = 443" | sudo tee -a /etc/stunnel/stunnel.conf
 echo "connect = 127.0.0.1:1194" | sudo tee -a /etc/stunnel/stunnel.conf
 echo "cert = /etc/stunnel/stunnel.pem" | sudo tee -a /etc/stunnel/stunnel.conf
-
 sudo sed -i -e 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 sudo cp /etc/stunnel/stunnel.pem ~
